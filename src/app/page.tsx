8c2aeafb5540be8af049d317bd6a116bcc477dd7
carlosbueno-mepyd <carlos.bueno@economia.gob.do>
2024-11-19 16:40:15 -0400

    fixing build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
+import '@/utils/i18'
 // Components
 import Presentation from '@/components/home/Presentation'
 import Knowledge from '@/components/home/Knowledge'
 import Skills from '@/components/home/Skills'
 import Projects from '@/components/home/Projects'
 import Experience from '@/components/home/Experience'
-import '@/utils/i18'
 import ContactUs from '@/components/home/ContactUs'
 
 export default function Home() {
